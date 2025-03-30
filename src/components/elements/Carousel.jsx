c66280ab012ab32fcc381b0e345c3696c8b80802
visibloom <visibloom2025@gmail.com>
2025-03-30 16:27:13 +0800

    services carousel details

--- src/components/elements/Carousel.jsx
+++ src/components/elements/Carousel.jsx
@@ -1,10 +1,13 @@
 export default function Carousel({carouselItem = []}) {
   return (
     <>
-      <div className="carousel rounded-box h-96">
+      <div className="carousel rounded-box">
         {carouselItem.map((src, index)=>(
-          <div key={index} className="carousel-item h-full">'
-            <img src={src} />
+          <div key={index} className="carousel-item h-full">
+            <div className="carousel-container">
+              <img src={src} />
+              <p className="carousel-details">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, blanditiis.</p>
+            </div>
           </div>
         ))}
       </div>
