e2e5fc9ece1bfdfd1ba3ac12c3c19ddb8391a53c
visibloom <visibloom2025@gmail.com>
2025-04-01 16:19:57 +0800

    services

--- src/components/util/images.js
+++ src/components/util/images.js
@@ -2,6 +2,7 @@
 import placeholder from "../../assets/placeholder.png"
 import placeholder2 from "../../assets/placeholder2.png"
 import business from "../../assets/businessmission.jpg"
+import graphicdesign from "../../assets/graphic-designing.png"
 
 export const images = {
     about:{
@@ -12,23 +13,23 @@ export const images = {
     carousel:{
         carousel1:[
             { 
-                src: "https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp",
+                src: graphicdesign,
                 details: "This is the details for carousel1, this carousel is still under development... please bare with me..."
             },
             { 
-                src: "https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp",
+                src: graphicdesign,
                 details: "This is the details for carousel2, this carousel is still under development... please bare with me..."
             },
             { 
-                src: "https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp",
+                src: graphicdesign,
                 details: "This is the details for carousel3, this carousel is still under development... please bare with me..."
             },
             { 
-                src: "https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp",
+                src: graphicdesign,
                 details: "This is the details for carousel4, this carousel is still under development... please bare with me..."
             },
             { 
-                src: "https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp",
+                src: graphicdesign,
                 details: "This is the details for carousel5, this carousel is still under development... please bare with me..."
             },
         ],
